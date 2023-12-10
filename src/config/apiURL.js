d4a696ddcb42c3e301dbdb7059ab1e0d04602655
RinshidKP <rinshid26@gmail.com>
2023-12-10 13:51:03 +0530

    updated url

--- src/config/apiURL.js
+++ src/config/apiURL.js
@@ -1,10 +1,16 @@
 // config.js
 export const apiUrls = {
-  studentUrl: 'http://localhost:3000/',
-  consultentUrl :'http://localhost:3000/consultent',
-  adminUrl : 'http://localhost:3000/admin',
-  chatUrl : 'http://localhost:3000/chat'
+  studentUrl: 'http://eduventure.live/',
+  consultentUrl :'http://eduventure.live/consultent',
+  adminUrl : 'http://eduventure.live/admin',
+  chatUrl : 'http://eduventure.live/chat'
   };
+// export const apiUrls = {
+//   studentUrl: 'http://localhost:3000/',
+//   consultentUrl :'http://localhost:3000/consultent',
+//   adminUrl : 'http://localhost:3000/admin',
+//   chatUrl : 'http://localhost:3000/chat'
+//   };
 
   export const baseImageUrl = 'http://localhost:3000/';
   
