d769541f7fb870041b487be3f8bfef723d78ed48
RinshidKP <rinshid26@gmail.com>
2023-12-14 14:20:37 +0530

    video player ratio changed

--- src/components/Chat/Message.jsx
+++ src/components/Chat/Message.jsx
@@ -119,7 +119,7 @@ const Message = ({recieverId, uniqueId, text, timestamp,type, isUser }) => {
     {type===1 &&
     <div className="group/items my-1">
         <div className=" h-28 w-52 max-h-32 " >
-          <Player fluid={false} height={360} width={640} >
+          <Player fluid={false} height={180} width={320} >
             <source src={`${baseImageUrl+text}`} />
           </Player >
         </div>
