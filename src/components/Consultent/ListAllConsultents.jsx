70503f8e3578216c761c7d1411e72a797d192d4f
RinshidKP <rinshid26@gmail.com>
2023-12-15 11:04:52 +0530

    started editing consultent list all page a

--- src/components/Consultent/ListAllConsultents.jsx
+++ src/components/Consultent/ListAllConsultents.jsx
@@ -94,38 +94,52 @@ const ListAllConsultents = () => {
                 </div>
             </div>
             <div className="w-full flex flex-wrap justify-center md:p-8">
-                {consultencies && consultencies.map((consultent) => (
-                    <div key={consultent._id} className="max-w-sm group/items my-3 md:mx-6 lg:mx-8 transition-transform hover:scale-105">
-                        <div className='bg-sky-100 rounded-b-lg'>
-                            <img
-                                className='object-cover rounded-t-lg'
-                                src={consultent.profile_image.url || defaultImage}
-                                alt=""
-                                style={{ width: '300px', height: '200px' }}
-                            />
-                            <div className='p-5'>
-                                <h5 className="my-1 text-xl sm:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    {consultent.consultancy_name}
-                                </h5>
-                                <p className="my-2 text-sm line-clamp-2 sm:text-base font-normal text-gray-700 dark:text-gray-400">
-                                    {consultent.description || 'Not Updated'}
-                                </p>
-                                <button
-                                    onClick={() => {
-                                        navigate(`/consultent_details`, { state: { consultent } })
-                                    }}
-                                    className="inline-flex items-center px-3 py-2 text-sm sm:text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 group-hover/items:bg-sky-700"
-                                >
-                                    Read more
-                                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                    </svg>
-                                </button>
+                {consultencies &&
+                    consultencies.map((consultent) => (
+                        <div
+                            key={consultent._id}
+                            className="w-full md:w-1/3 lg:w-1/4 xl:w-1/4 group/items my-3 md:mx-6 lg:mx-8 transition-transform hover:scale-105"
+                        >
+                            <div className="bg-sky-100 rounded-b-lg">
+                                <img
+                                    className="object-cover rounded-t-lg"
+                                    src={consultent.profile_image.url || defaultImage}
+                                    alt=""
+                                />
+                                <div className="p-5">
+                                    <h5 className="my-1 text-xl sm:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        {consultent.consultancy_name}
+                                    </h5>
+                                    <p className="my-2 text-sm line-clamp-2 sm:text-base font-normal text-gray-700 dark:text-gray-400">
+                                        {consultent.description || 'Not Updated'}
+                                    </p>
+                                    <button
+                                        onClick={() => {
+                                            navigate(`/consultent_details`, { state: { consultent } });
+                                        }}
+                                        className="inline-flex items-center px-3 py-2 text-sm sm:text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 group-hover/items:bg-sky-700"
+                                    >
+                                        Read more
+                                        <svg
+                                            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                                            aria-hidden="true"
+                                            viewBox="0 0 14 10"
+                                        >
+                                            <path
+                                                stroke="currentColor"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M1 5h12m0 0L9 1m4 4L9 9"
+                                            />
+                                        </svg>
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
             </div>
+
 
             {/* Pagination controls */}
             <div className="flex justify-center my-6 sm:my-8 md:my-10">
